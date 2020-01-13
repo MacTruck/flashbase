2a82936dfe25e3da679b25f792b5a5b176763994
Scott McInnis <scottalyst@gmail.com>
2020-01-13 00:58:26 -0800

    Converted from styled-components to emotion package

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { css } from 'emotion';
+
+const footerStyle = css`
+  position: fixed;
+  bottom: 0;
+  padding: 20px 0;
+  text-align: center;
+  width: 100%;
+  border-top: 1px solid rgba(0,0,0,.1);
+`;
 
 const Footer = () => {
   return (
-    <footer>This is the footer</footer>
+    <footer className={footerStyle}>This is the footer</footer>
   );
 }
 
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import { css } from 'emotion';
+
+const headerStyle = css`
+  display: flex;
+  padding: 20px;
+  background: #fff;
+  border-bottom: 1px solid rgba(0,0,0,.1);
+  box-shadow: 0 0 10px rgba(0,0,0,.125);
+`;
+
+const headerLeft = css`
+  width: 33.33%;
+  text-align: left;
+`;
+
+const headerCenter = css`
+  width: 33.33%;
+  text-align: center;
+`;
+
+const headerRight = css`
+  width: 33.33%;
+  text-align: right;
+`;
 
 const Header = () => {
   return (
-    <header>This is the header</header>
+    <React.Fragment>
+      <header className={headerStyle}>
+        <div className={headerLeft}>Left</div>
+        <div className={headerCenter}>Center</div>
+        <div className={headerRight}>Right</div>
+      </header>
+    </React.Fragment>
   );
 }
 
